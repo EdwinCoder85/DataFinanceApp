a4ce0d191db71404e9c541d4b7a5e4c0a6bb75d8
Edwin Amaranto Villegas <63481204+EdwinCoder85@users.noreply.github.com>
2024-01-04 01:58:43 -0500

    fourth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 // import Typed from "react-typed";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
   return (
@@ -15,16 +16,22 @@ const Hero = () => {
           <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
             Fast, flexible financing for
           </p>
-          {/* <Typed
+          <Typewriter
             className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
-            strings={["BTB", "BTC", "SASS"]}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          /> */}
+            options={{
+              strings: ["BTB", "BTC", "SASS"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
-        <p className="md:text-2xl text-xl font-bold text-gray-500">Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms</p>
-        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Get Started</button>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your data analytics to increase revenue for BTB, BTC, & SASS
+          platforms
+        </p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+          Get Started
+        </button>
       </div>
     </div>
   );
